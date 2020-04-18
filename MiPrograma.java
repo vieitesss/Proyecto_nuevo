c9cb9b80ac885c5e4fb23f2faca0cc800f833c43
Dani <dvieitest@gmail.com>
2020-04-18 13:58:25 +0200

    Tercer commit

--- MiPrograma.java
+++ MiPrograma.java
@@ -1,5 +1,9 @@
 public class MiPrograma{
     public static void main(String [] args){
         System.out.println("Hola 1");
+
+        for(int i = 0; i < 10; i++){
+            System.out.println(i);
+        }
     }
 }
